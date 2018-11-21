88a90cd6afe1df8692f6415db8b5fe3e483f6505
dandan53 <dandan53@gmail.com>
2018-11-21 19:41:06 +0200

    AlarmActivity

--- app/src/main/java/com/example/dandan/mobileapp/AlarmActivity.java
+++ app/src/main/java/com/example/dandan/mobileapp/AlarmActivity.java
@@ -1,11 +1,15 @@
 package com.example.dandan.mobileapp;
 
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -49,52 +53,61 @@ public class AlarmActivity extends AppCompatActivity {
     protected void onCreate(android.os.Bundle r3) {
 
         super.onCreate(r3);
+        setContentView(R.layout.activity_alarm);
 
+        Intent intent = getIntent();
+        String r0 = "EXIT";
+        boolean r1 = false;
+        boolean bo = intent.getBooleanExtra(r0, r1);
+        if (!bo) {
+            finish();
+            return;
+        } else {
+            initControls();
+            Window window = getWindow();
+            int f0 = 4194304; // 0x400000 float:5.877472E-39 double:2.0722615E-317;
+            window.addFlags(f0);
+            int f1 = 524288; // 0x80000 float:7.34684E-40 double:2.590327E-318;
+            window.addFlags(f1);
+            int f3 = 2097152; // 0x200000 float:2.938736E-39 double:1.0361308E-317;
+            window.addFlags(f3);
+            startAlarmSound();
+            Intent intent1 = getIntent();
+            String packName = "AlarmAppPackageName";
+            String strExtra = intent1.getStringExtra(packName);
+            if (strExtra == null) {
+                return;
+            } else {
+                _alarmedAppPackageName = strExtra;
+                PackageManager packageManager = getPackageManager();
 
-        /*
-        r2 = this;
-        super.onCreate(r3);
-        r3 = 2131361819; // 0x7f0a001b float:1.8343401E38 double:1.0530326536E-314;
-        r2.setContentView(r3);
-        r3 = r2.getIntent();
-        r0 = "EXIT";
-        r1 = 0;
-        r3 = r3.getBooleanExtra(r0, r1);
-        if (r3 == 0) goto L_0x001a;
-    L_0x0016:
-        r2.finish();
-        return;
-    L_0x001a:
-        r2.initControls();
-        r3 = r2.getWindow();
-        r0 = 4194304; // 0x400000 float:5.877472E-39 double:2.0722615E-317;
-        r3.addFlags(r0);
-        r0 = 524288; // 0x80000 float:7.34684E-40 double:2.590327E-318;
-        r3.addFlags(r0);
-        r0 = 2097152; // 0x200000 float:2.938736E-39 double:1.0361308E-317;
-        r3.addFlags(r0);
-        r2.startAlarmSound();
-        r3 = r2.getIntent();
-        r0 = "AlarmAppPackageName";
-        r3 = r3.getStringExtra(r0);
-        if (r3 == 0) goto L_0x0065;
-    L_0x003f:
-        r2._alarmedAppPackageName = r3;
-        r3 = r2.getPackageManager();
-        r0 = r2._alarmedAppPackageName;	 Catch:{ Exception -> 0x0065 }
-        r1 = 128; // 0x80 float:1.794E-43 double:6.32E-322;	 Catch:{ Exception -> 0x0065 }
-        r0 = r3.getApplicationInfo(r0, r1);	 Catch:{ Exception -> 0x0065 }
-        r1 = r2._txtApp;	 Catch:{ Exception -> 0x0065 }
-        r0 = r3.getApplicationLabel(r0);	 Catch:{ Exception -> 0x0065 }
-        r0 = r0.toString();	 Catch:{ Exception -> 0x0065 }
-        r1.setText(r0);	 Catch:{ Exception -> 0x0065 }
-        r0 = r2._alarmedAppPackageName;	 Catch:{ Exception -> 0x0065 }
-        r3 = r3.getApplicationIcon(r0);	 Catch:{ Exception -> 0x0065 }
-        r0 = r2._imgAppIcon;	 Catch:{ Exception -> 0x0065 }
-        r0.setImageDrawable(r3);	 Catch:{ Exception -> 0x0065 }
-    L_0x0065:
-        return;
-        */
+                ApplicationInfo applicationInfo = null;
+
+                try {
+                    applicationInfo = packageManager.getApplicationInfo(this._alarmedAppPackageName, PackageManager.GET_META_DATA);
+                } catch (PackageManager.NameNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                if (applicationInfo != null) {
+                    CharSequence appLabel = packageManager.getApplicationLabel(applicationInfo);
+                    String appLabelStr = appLabel.toString();
+                    _txtApp.setText(appLabelStr);
+
+                    Drawable appIcon = null;
+
+                    try {
+                        appIcon = packageManager.getApplicationIcon(_alarmedAppPackageName);
+                    } catch (PackageManager.NameNotFoundException e) {
+                        e.printStackTrace();
+                    }
+
+                    if (appIcon != null) {
+                        _imgAppIcon.setImageDrawable(appIcon);
+                    }
+                }
+            }
+        }
     }
 
     protected void onDestroy() {
